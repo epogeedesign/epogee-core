a6e7989289354dc1d050dc204e93b65af9f35ad0
Epogee Design <54156867+epogeedesign@users.noreply.github.com>
2022-10-06 13:06:10 -0500

    Add plugin main file

---
+++ epogee-core.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+Plugin Name:  Epogee Core
+Description:  Enhanced functions and settings for WordPress.
+Version:      1.1.0
+Author URI:   https://epogeedesign.com/
+*/
+
+$ep_folder = dirname(__FILE__);
+
+// Composer Autoload
+require_once 'vendor/autoload.php';
+
+// Core Classes
+require_once 'inc/classes/post.class.php';
+require_once 'inc/classes/term.class.php';
+require_once 'inc/classes/options.class.php';
+require_once 'inc/classes/admin-page.class.php';
+require_once 'inc/classes/admin-tab.class.php';
+
+// Functions
+require_once 'inc/functions/options.inc.php';
+require_once 'inc/functions/templates.inc.php';
+require_once 'inc/functions/filters.inc.php';
+require_once 'inc/functions/rest.inc.php';
+require_once 'inc/functions/fields.inc.php';
+require_once 'inc/functions/posts.inc.php';
+require_once 'inc/functions/terms.inc.php';
+require_once 'inc/functions/imgix.inc.php';
+require_once 'inc/functions/yoast.inc.php';
+
+// WordPress Filters
+require_once 'inc/filters/updates.inc.php';
+require_once 'inc/filters/editor.inc.php';
+require_once 'inc/filters/comments.inc.php';
+require_once 'inc/filters/imgix.inc.php';
+require_once 'inc/filters/scripts.inc.php';
+require_once 'inc/filters/phpmailer.inc.php';
+require_once 'inc/filters/404-page.inc.php';
+require_once 'inc/filters/last-login.inc.php';
+
+// Epogee Options
+require_once 'inc/options/options.inc.php';
+
+// Custom Fields Types
+require_once 'inc/fields/fields.inc.php';
